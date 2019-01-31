edace0ff24a7316d4a4667d0ee25bd8e81ae62c0
Lorenz Cheung <lorenz.cheung@gmail.com>
2019-01-31 16:39:18 +0800

    ignore more stuff

--- game-controller/src/main/java/org/scrum/psd/battleship/controller/dto/Color.java
+++
@@ -1,5 +0,0 @@
-package org.scrum.psd.battleship.controller.dto;
-
-public enum Color {
-    CADET_BLUE, CHARTREUSE, ORANGE, RED, YELLOW;
-}
--- game-controller/src/main/java/org/scrum/psd/battleship/controller/dto/Letter.java
+++
@@ -1,5 +0,0 @@
-package org.scrum.psd.battleship.controller.dto;
-
-public enum Letter {
-    A, B, C, D, E, F, G, H;
-}
